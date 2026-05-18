02556d6efdd6b9298f450d6199a2d03b5caf76ff
Jose Branco <161507217+josebranco13@users.noreply.github.com>
2026-05-19 00:48:51 +0100

    mundanças

--- KubernetesController/KubernetesController/Form1.cs
+++ KubernetesController/KubernetesController/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KubernetesController.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,17 @@ namespace KubernetesController
 {
     public partial class Form1 : Form
     {
+        private KubernetesApiClient api;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /// MUDAR IP AQUI!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            api = new KubernetesApiClient("http://IP_DO_MASTER_K3S:8001");
         }
     }
 }
---
+++ KubernetesController/KubernetesController/Services/KubernetesApiClient.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace KubernetesController.Services
+{
+    internal class KubernetesApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public KubernetesApiClient(string baseUrl)
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(baseUrl.TrimEnd('/') + "/")
+            };
+        }
+
+        public async Task<string> GetAsync(string endpoint)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(endpoint.TrimStart('/'));
+            string content = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+                throw new Exception($"Erro GET {endpoint}: {response.StatusCode}\n{content}");
+
+            return content;
+        }
+
+        public async Task<string> PostAsync(string endpoint, string jsonBody)
+        {
+            var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await _httpClient.PostAsync(endpoint.TrimStart('/'), content);
+            string responseContent = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+                throw new Exception($"Erro POST {endpoint}: {response.StatusCode}\n{responseContent}");
+
+            return responseContent;
+        }
+
+        public async Task<string> DeleteAsync(string endpoint)
+        {
+            HttpResponseMessage response = await _httpClient.DeleteAsync(endpoint.TrimStart('/'));
+            string content = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+                throw new Exception($"Erro DELETE {endpoint}: {response.StatusCode}\n{content}");
+
+            return content;
+        }
+    }
+}
